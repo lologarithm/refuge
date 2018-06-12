fdbdf2930e46ba0a9623eca636df1ad1414de1d0
Ben Echols <lologarithm@gmail.com>
2018-06-12 00:43:48 -0600

    threw in an http server because i can

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"runtime/debug"
+	"sync"
 	"time"
 
 	rpio "github.com/stianeikeland/go-rpio"
@@ -21,26 +24,78 @@ func main() {
 		fmt.Printf("no input pin set.")
 		os.Exit(1)
 	}
+	stream := runTherm(*pinN)
+	mutex := sync.Mutex{}
+	data := make([]env, 1440)
+	var latest env
+	i := 0
+	go func() {
+		for d := range stream {
+			// fmt.Printf("Temp: %.1fC(%dF) Humidity: %.1f%%  I:%d\n", d.temp, int(d.temp*9/5)+32, d.hum, i)
+			mutex.Lock()
+			latest = d
+			data[i] = d
+			i++
+			if i == len(data) {
+				i = 0
+			}
+			mutex.Unlock()
+		}
+		fmt.Printf("exiting data processor")
+	}()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mutex.Lock()
+		d := latest
+		mutex.Unlock()
+		w.Write([]byte(fmt.Sprintf("<html><body style=\"font-size: 2em;\"><p><b>At:</b> %s</p><p><b>Temp:</b> %.1fC(%dF)</p><p><b>Humidity:</b> %.1f%%</p></html></body>", d.t.Format("Mon Jan _2 15:04:05 2006"), d.temp, int(d.temp*9/5)+32, d.hum)))
+	})
+
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type env struct {
+	temp float32
+	hum  float32
+	t    time.Time
+}
+
+func runTherm(p int) chan env {
 	err := rpio.Open()
 	if err != nil {
 		fmt.Printf("Failed to open pins: %s\n", err)
 		os.Exit(1)
 	}
 
-	pin := rpio.Pin(22)
+	pin := rpio.Pin(p)
 
-	var t, h float32
-	var csg bool
-	debug.SetGCPercent(-1)
-	for i := 0; i < 10; i++ {
-		t, h, csg = readDHT22(pin)
-		if csg {
-			break
+	stream := make(chan env, 10)
+	go func() {
+		for {
+			var t, h float32
+			var csg bool
+			debug.SetGCPercent(-1)
+			for i := 0; i < 10; i++ {
+				t, h, csg = readDHT22(pin)
+				if csg {
+					break
+				}
+			}
+			debug.SetGCPercent(100)
+			// fmt.Printf("Temp: %.1fC(%dF) Humidity: %.1f%%\n", t, int(t*9/5)+32, h)
+			select {
+			case stream <- env{temp: t, hum: h, t: time.Now()}:
+				// good!
+			default:
+				return // bad, exit
+			}
+			time.Sleep(time.Second * 60)
 		}
-	}
-	debug.SetGCPercent(100)
-
-	fmt.Printf("Temp: %.1fC(%dF) Humidity: %.1f%%\n", t, int(t*9/5)+32, h)
+	}()
+	return stream
 }
 
 func readDHT22(pin rpio.Pin) (float32, float32, bool) {
